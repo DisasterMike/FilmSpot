04b6f707f7458787130c0653f81e4cf99978862f
michael stump <maikerustump@gmail.com>
2024-07-13 16:36:54 +0900

    added model validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,11 @@
 class Bookmark < ApplicationRecord
   belongs_to :users
   belongs_to :spots
+
+  # Note validations
+  validates :note, presence: true
+
+  # Priority validations
+  validates :priority, presence: true
+  validates :priority, numericality: { only_integer: true }
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,4 +2,18 @@ class Spot < ApplicationRecord
   belongs_to :users
   has_many :bookings
   has_many :bookmarks
+
+  # name validations
+  validates :name, presence: true
+  validates :name, uniqueness: true # TODO: do we need this?
+
+  # address validations
+  validates :address, presence: true
+
+  # description validations
+  validates :description, presence: true
+
+  # category validations
+  validates :category, presence: true
+  validates :category, inclusion: %w[park restaurant house other] # TODO: maybe change this later?
 end
