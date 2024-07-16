48746cdb0c6d929008ebb99d95e1fd793dd6fe4a
Seisuke Fukuda <razerrmc163@gmail.com>
2024-07-16 20:02:19 +0900

    added controller and new html erb file

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -6,8 +6,21 @@ class SpotsController < ApplicationController
   end
 
   def new
+    @spot = Spot.new
   end
 
   def create
+    @spot = Spot.new(spots_params)
+    if @spot.save
+      redirect_to owner_spots_show_path(@spot)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def spots_params
+    param.srequire(:spot).permit(:name, :address, :description, :category, :daily_rate)
   end
 end
