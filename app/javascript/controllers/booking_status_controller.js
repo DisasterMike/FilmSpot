e3f50f4c835ef2b7cb9854c500f397968af6088f
michael stump <maikerustump@gmail.com>
2024-07-19 23:20:35 +0900

    separated filter logic into different controller

--- app/javascript/controllers/booking_status_controller.js
+++ app/javascript/controllers/booking_status_controller.js
@@ -5,47 +5,10 @@ const defaultBackgroundClass = "btn-dark";
 const selectedBackgroundClass = "btn-warning";
 
 export default class extends Controller {
-  static targets = [ "all", "pending", "accepted", "declined", "statusCard", "statusWord", "bookingsRow", "filterBox", "currentFilter" ]
+  static targets = [ "all", "pending", "accepted", "declined", "statusCard", "statusWord", "bookingsRow" ]
 
   connect() {
     // console.log("hello from the booking status controller");
-  }
-
-  enableFilterList(event){
-    // console.log("enable");
-    this.filterBoxTarget.classList.toggle("d-none");
-  }
-
-  disableFilterList(){
-    // console.log("disable");
-    this.filterBoxTarget.classList.add("d-none");
-  }
-
-  filterType(event){
-    // console.log(event.currentTarget.innerText);
-    this.currentFilterTarget.innerText = event.currentTarget.innerText
-    // TODO - change the cards order!
-  }
-
-  filterByName(){
-    // do a fetch request into the database,
-    // then remove all current cards
-    // repopulate the cards with SORTED array of new cards from the database
-
-    this.statusCardTargets.forEach(card => {
-      // card.remove();
-    });
-
-    fetch("http://localhost:3000/owner/bookings/all")
-      .then(response => response.json())
-      .then(data => {
-        data.forEach(element => {
-          console.log(element);
-          // this.bookingsRowTarget.insertAdjacentHTML("beforeend",
-          //   "<h2>test</h2>"
-          // )
-        });
-      })
   }
 
   // All Button
